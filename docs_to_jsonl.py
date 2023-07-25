4bb273f44a42aae873cfe8a7e89db60966ce2439
K-Singleton <kesingleton2021@gmail.com>
2023-07-25 16:05:34 -0400

    Converted each question and answer to a json object

--- docs_to_jsonl.py
+++ docs_to_jsonl.py
@@ -10,15 +10,15 @@ def main():
     howto_dict['../kb_sdk_docs/source/howtos/dynamic_services.rst'] = 'How do I create a dynamic service for an SDK module?'
     howto_dict['../kb_sdk_docs/source/howtos/edit_your_dockerfile.rst'] = 'How do I edit the dockerfile of a KBase app?'
 
-    howto_dict['../kb_sdk_docs/source/howtos/file_utils.rst'] = 'value1'
-    howto_dict['../kb_sdk_docs/source/howtos/fill_out_app_information.rst'] = 'value1'
-    howto_dict['../kb_sdk_docs/source/howtos/job_manager.rst'] = 'value1'
-    howto_dict['../kb_sdk_docs/source/howtos/manager_build.rst'] = 'value1'
+    howto_dict['../kb_sdk_docs/source/howtos/file_utils.rst'] = 'How do I acess the utilities of a file?'
+    howto_dict['../kb_sdk_docs/source/howtos/fill_out_app_information.rst'] = 'How do I complete the documentation for a KBase app?'
+    howto_dict['../kb_sdk_docs/source/howtos/job_manager.rst'] = 'How do I list and view the jobs running on KBase?'
+    howto_dict['../kb_sdk_docs/source/howtos/manager_build.rst'] = 'How do I manually build the KBase SDK?'
 
-    howto_dict['../kb_sdk_docs/source/howtos/run_a_shell_command.rst'] = 'value1'
-    howto_dict['../kb_sdk_docs/source/howtos/update_to_py3.rst'] = 'value1'
-    howto_dict['../kb_sdk_docs/source/howtos/workspace.rst'] = 'value1'
-    howto_dict['../kb_sdk_docs/source/howtos/work_with_reference_data.rst'] = 'value1'
+    howto_dict['../kb_sdk_docs/source/howtos/run_a_shell_command.rst'] = 'How do I run shell commands in the dockerfile of a KBase app?'
+    howto_dict['../kb_sdk_docs/source/howtos/update_to_py3.rst'] = 'How do I update a KBase module to Python 3?'
+    howto_dict['../kb_sdk_docs/source/howtos/workspace.rst'] = 'How do I add reference data to a KBase app?'
+    howto_dict['../kb_sdk_docs/source/howtos/work_with_reference_data.rst'] = 'How do I retrieve the workspace object from a KBase narrative?'
 
     kbase_files = pathlib.Path("../kb_sdk_docs/source/howtos")
     howtos = (list(kbase_files.iterdir()))
@@ -28,10 +28,10 @@ def main():
     for howto in howtos:
         with open(howto) as f:
             answer = f.read()
-        for key,value in howto_dict.items():
-            text = f'### Human: {key}### Assistant: {value}'
+    for key,value in howto_dict.items():
+        text = f'### Human: {value}### Assistant: {key}'
         # dict(foo=100, bar=200)
-            line = json.dumps(dict(text=text))
+        line = json.dumps(dict(text=text))
         with open("data.jsonl","a") as f:
             f.write(f'{line}\n')
    
