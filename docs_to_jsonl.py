e95ee89bff180405e40e3725fe446345219812e9
K-Singleton <135647970+K-Singleton@users.noreply.github.com>
2023-07-26 17:20:47 -0400

    made changes to dictionary

--- docs_to_jsonl.py
+++ docs_to_jsonl.py
@@ -4,21 +4,20 @@ import json
 import pathlib
 
 def main():
-    howto_dict = {}
-    howto_dict['../kb_sdk_docs/source/howtos/add_ui_elements.rst'] = 'How do I add a ui element to a KBase module?'
-    howto_dict['../kb_sdk_docs/source/howtos/create_a_report.rst'] = 'How do I create a report for a KBase app?'
-    howto_dict['../kb_sdk_docs/source/howtos/dynamic_services.rst'] = 'How do I create a dynamic service for an SDK module?'
-    howto_dict['../kb_sdk_docs/source/howtos/edit_your_dockerfile.rst'] = 'How do I edit the dockerfile of a KBase app?'
-
-    howto_dict['../kb_sdk_docs/source/howtos/file_utils.rst'] = 'How do I acess the utilities of a file?'
-    howto_dict['../kb_sdk_docs/source/howtos/fill_out_app_information.rst'] = 'How do I complete the documentation for a KBase app?'
-    howto_dict['../kb_sdk_docs/source/howtos/job_manager.rst'] = 'How do I list and view the jobs running on KBase?'
-    howto_dict['../kb_sdk_docs/source/howtos/manager_build.rst'] = 'How do I manually build the KBase SDK?'
-
-    howto_dict['../kb_sdk_docs/source/howtos/run_a_shell_command.rst'] = 'How do I run shell commands in the dockerfile of a KBase app?'
-    howto_dict['../kb_sdk_docs/source/howtos/update_to_py3.rst'] = 'How do I update a KBase module to Python 3?'
-    howto_dict['../kb_sdk_docs/source/howtos/workspace.rst'] = 'How do I add reference data to a KBase app?'
-    howto_dict['../kb_sdk_docs/source/howtos/work_with_reference_data.rst'] = 'How do I retrieve the workspace object from a KBase narrative?'
+    questions = {
+        '../kb_sdk_docs/source/howtos/add_ui_elements.rst': 'How do I add a ui element to a KBase module?',
+        '../kb_sdk_docs/source/howtos/create_a_report.rst': 'How do I create a report for a KBase app?',
+        '../kb_sdk_docs/source/howtos/dynamic_services.rst': 'How do I create a dynamic service for an SDK module?',
+        '../kb_sdk_docs/source/howtos/edit_your_dockerfile.rst': 'How do I edit the dockerfile of a KBase app?',
+        '../kb_sdk_docs/source/howtos/file_utils.rst': 'How do I acess the utilities of a file?',
+        '../kb_sdk_docs/source/howtos/fill_out_app_information.rst': 'How do I complete the documentation for a KBase app?',
+        '../kb_sdk_docs/source/howtos/job_manager.rst': 'How do I list and view the jobs running on KBase?',
+        '../kb_sdk_docs/source/howtos/manual_build.rst': 'How do I manually build the KBase SDK?',
+        '../kb_sdk_docs/source/howtos/run_a_shell_command.rst': 'How do I run shell commands in the dockerfile of a KBase app?',
+        '../kb_sdk_docs/source/howtos/update_to_py3.rst': 'How do I update a KBase module to Python 3?',
+        '../kb_sdk_docs/source/howtos/workspace.rst': 'How do I add reference data to a KBase app?',
+        '../kb_sdk_docs/source/howtos/work_with_reference_data.rst': 'How do I retrieve the workspace object from a KBase narrative?',
+    }
 
     kbase_files = pathlib.Path("../kb_sdk_docs/source/howtos")
     howtos = (list(kbase_files.iterdir()))
@@ -28,8 +27,7 @@ def main():
     for howto in howtos:
         with open(howto) as f:
             answer = f.read()
-    for key,value in howto_dict.items():
-        text = f'### Human: {value}### Assistant: {key}'
+        text = f'### Human: {questions[str(howto)]}### Assistant: {answer}'
         # dict(foo=100, bar=200)
         line = json.dumps(dict(text=text))
         with open("data.jsonl","a") as f:
